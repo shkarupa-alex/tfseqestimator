17eb3ffb15635ab94961e03fee1083b4874ca8ba
Алексей Шкарупа <shkarupa.alex@gmail.com>
2018-07-25 16:00:45 +0300

    Version changed

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='tfseqestimator',
-    version='1.1.0',
+    version='2.0.0',
     description='Sequence estimators for Tensorflow',
     long_description=long_description,
     long_description_content_type="text/markdown",
